b1f4d36aca9fde959a1c6eaae8ac33f499dbd35c
unknown <aelise17264@gmail.com>
2020-08-14 11:35:31 -0600

    styles complete

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,23 +1,57 @@
 // Write your Character component here
 import React from 'react';
 import Planets from './Planets'
+import styled, {keyframes} from 'styled-components'
 
 const Character = ({info}) => {
    
+    const StyledContainer = styled.div`
+    opacity: .7;
+    border: solid 5px black;
+    border-bottom: none;
+    width: 75vw;
+    margin-left: 10%;
+    margin-bottom: 0;
+    padding: 0;
+    padding-bottom: 1%;
+    padding-right: 4%;
+   
+    `
+
+    const StyledCharacter = styled.div`
+    display: flex;
+    opacity: .7;
+    border: solid 5px black;
+    border-top: none;
+    width: 75vw;
+    align-items: center;
+    margin-left: 10%;
+    margin-top: 0;
+    font-family: 'Source Code Pro';
+    justify-content: space-between;
+    padding-bottom: 2%;
+    padding-top: 0;
+    padding-left: 2%;
+    padding-right: 2%;
+    `
+
     return (
-      <div className="character">
-          <h1 className='details'>Character Details</h1>
+        <>
+        <StyledContainer className='container'>
         <h2>Character Name: {info.name}</h2>
-         <h4>Height: {info.height}</h4>
-         <h4>Mass: {info.mass}</h4>
-        <h4>Hair Color: {info.hair_color}</h4>
-        <h4>Skin Color: {info.skin_color}</h4>
-        <h4>Eye Color:{info.eye_color} </h4>
-    <h4>Birth Year: {info.birth_year}</h4>
-        <h4>Gender: {info.gender}</h4>
-        <Planets/>
+        </StyledContainer>
+      <StyledCharacter className="character">
        
-      </div>
+         <h3>Height: {info.height}</h3>
+         <h3>Mass: {info.mass}</h3>
+        <h3>Hair Color: {info.hair_color}</h3>
+        <h3>Skin Color: {info.skin_color}</h3>
+        <h3>Eye Color:{info.eye_color} </h3>
+    <h3>Birth Year: {info.birth_year}</h3>
+        <h3>Gender: {info.gender}</h3>       
+      </StyledCharacter>
+     
+      </>
     );
   }
   
--- starwars/src/components/Planet.js
+++ starwars/src/components/Planet.js
@@ -1,20 +1,11 @@
 import React from 'react';
 
 const Planet = ({info}) => {
-   
-   function toggleButton(id){
-       let details = document.getElementById('planetName')
-       if(details.style.display == 'block'){
-           details.style.display = 'none'
-       }else{
-           details.style.display = 'block'
-       }
-   }
+  
 
     return (
       <>
-      <button onClick='toggleButton()'>Home Planet Info</button>
-      <div id='planetName' style='display: none'>
+      <div id='planetName'>
         <h4>Home Planet: {info.name}</h4>
     <h5>Climate: {info.climate}</h5>
     <h5>Population: {info.population}</h5>
